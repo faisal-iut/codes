da5d4c3eb7eac938900c1f0f1c31161c53502a6a
faisal <fahimfaisal@iut-dhaka.edu>
2017-05-08 14:54:52 +0600

    Revert "added code files on 1st swc training ending"
    This reverts commit a48d8937f33d31beae279a1d9fcc70af69be8284.

--- temp.cpp
+++ temp.cpp
@@ -1,31 +1,18 @@
-#include<iostream>
-#include<cstdio>
-#include<malloc.h>
+#include <iostream>
 using namespace std;
-
-struct node
-{
-    char data;
-    struct node* next;
-};
-
 
 int main()
 {
-    freopen("input.txt","r",stdin);
-    struct node *current, *prev, *head;
-    int length = 0;
-    char c, *input, *f;
-    input = (char *) malloc(sizeof(char));
-    while((c=getchar())!=EOF)
-    {
-        realloc(input,sizeof(char));
-        input[length]=c;
-        length++;
-    }
-    length = length -1;
-    free(input+length);
-    printf("%s",input);
+	char sentence[5][3] ;
 
+	for ( int i = 0; i < 2; i++ )
+	{
+
+		cin>> sentence[i];
+	}
+
+	for ( int i = 0; i < 2; i++ )
+	{
+		cout << sentence[i] << "\n";
+	}
 }
-
